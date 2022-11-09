048f96f366bbf8d153488c0f1dff67ecb243190f
Lucas Palavecino <86084379+Luquinuts@users.noreply.github.com>
2022-11-09 12:31:09 -0300

    Update empresa.sql

--- database/empresa.sql
+++ database/empresa.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-08-2022 a las 23:39:53
+-- Tiempo de generación: 09-11-2022 a las 16:29:34
 -- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.4.10
+-- Versión de PHP: 7.4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,29 +28,18 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `clientes` (
-  `idClientes` int(11) NOT NULL,
-  `NombreCliente` varchar(128) DEFAULT NULL,
-  `ApellidoCliente` varchar(128) DEFAULT NULL,
-  `FechaNacCliente` date NOT NULL,
-  `PesoCliente` float DEFAULT NULL,
-  `AlturaCliente` float DEFAULT NULL,
-  `DomicilioCliente` varchar(128) DEFAULT NULL,
+  `IdClientes` int(11) NOT NULL,
+  `Nombre` varchar(128) DEFAULT NULL,
+  `Apellido` varchar(128) DEFAULT NULL,
+  `FechaNacimiento` date NOT NULL,
+  `Peso` float DEFAULT NULL,
+  `Altura` float DEFAULT NULL,
+  `Domicilio` varchar(128) DEFAULT NULL,
   `CodPostal` int(11) DEFAULT NULL,
-  `Movil01Cliente` bigint(11) DEFAULT NULL,
-  `Movil02Cliente` bigint(11) DEFAULT NULL,
-  `EmailCliente` varchar(128) DEFAULT NULL
+  `Movil01` bigint(11) DEFAULT NULL,
+  `Movil02` bigint(11) DEFAULT NULL,
+  `Email` varchar(128) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `clientes`
---
-
-INSERT INTO `clientes` (`idClientes`, `NombreCliente`, `ApellidoCliente`, `FechaNacCliente`, `PesoCliente`, `AlturaCliente`, `DomicilioCliente`, `CodPostal`, `Movil01Cliente`, `Movil02Cliente`, `EmailCliente`) VALUES
-(1, 'Lucas', 'Forchino', '1979-01-24', 95.5, 1.8, 'Jujuy 1234', 7600, 2236121212, NULL, 'lucas@gmail.com'),
-(2, 'Jorge', 'Solis', '1945-10-01', 78.2, 1.8, 'Almafuerte 321', 8000, 2291614593, 2291614593, 'j@hotmail.com'),
-(3, 'Javier', 'Fernandez', '1975-09-02', 90, 1.77, 'Av. Paso 100', 7600, 2235444444, 2235444445, 'javier@gmail.com'),
-(23, 'Jorge', 'Solisa', '1982-01-01', 100, 1.8, 'Av. Colon 4444', 7600, 22351666666, 22351666667, 'sol@gmail.com'),
-(35, 'Juan', 'Mercado', '1964-02-02', 89.6, 1.77, 'Av. Independeica 720', 7600, 2236166744, 2236166745, 'mercado@gmail.com');
 
 --
 -- Índices para tablas volcadas
@@ -60,7 +49,7 @@ INSERT INTO `clientes` (`idClientes`, `NombreCliente`, `ApellidoCliente`, `Fecha
 -- Indices de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  ADD PRIMARY KEY (`idClientes`);
+  ADD PRIMARY KEY (`IdClientes`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -70,7 +59,7 @@ ALTER TABLE `clientes`
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `idClientes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `IdClientes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
